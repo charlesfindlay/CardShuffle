e2fbc41d74392de92d234a562405af393d5e506a
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-07-09 11:39:16 -0400

    Shuffle and deal cards
    Shuffles a deck and then deals a single hand

---
+++ CardShuffle/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CardShuffle
+{
+    public class Hand
+    {
+        public string card1 { get; set; }
+        public string card2 { get; set; }
+        public string card3 { get; set; }
+        public string card4 { get; set; }
+        public string card5 { get; set; }
+
+
+
+        public Hand()
+        {
+            
+        }
+
+        public void GetCards(List<string> mydeck)
+        {
+            card1 = mydeck.ElementAt(0);
+            card2 = mydeck.ElementAt(1);
+            card3 = mydeck.ElementAt(2);
+            card4 = mydeck.ElementAt(3);
+            card5 = mydeck.ElementAt(4);
+            mydeck.RemoveRange(0, 5);
+        }
+    }
+}
+
+
---
+++ CardShuffle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XnaFan;
+
+namespace CardShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHand = new Hand();
+            var mydeck = CreateDeck();
+            mydeck.Shuffle();
+            myHand.GetCards(mydeck);
+
+            Console.WriteLine(myHand.card1);
+            Console.WriteLine(myHand.card2);
+            Console.WriteLine(myHand.card3);
+            Console.WriteLine(myHand.card4);
+            Console.WriteLine(myHand.card5);
+            
+
+            Console.ReadLine();
+        }
+
+        
+        public static List<string> CreateDeck()
+        {
+            string[] values = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            string[] suits = { "H", "D", "C", "S" };
+            List<string> newDeck = new List<string>();
+            foreach (string suit in suits)
+            {
+                foreach (string value in values)
+                {
+                    string card = value + suit;
+                    newDeck.Add(card);
+                }
+            }
+            return newDeck;
+        }
+
+
+        //private static List<string> DealCards(Deck myDeck)
+        //{
+            
+
+        //    return;
+        //}
+    }
+}
